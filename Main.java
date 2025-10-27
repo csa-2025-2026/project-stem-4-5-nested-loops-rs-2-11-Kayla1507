98e0471435320361d5c93e12bf3d448649713c3f
per5 <klapham007@gmail.com>
2025-10-27 21:46:19 +0000

    2

--- Main.java
+++ Main.java
@@ -4,17 +4,35 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Make sure to test your methods here
+    printNTimes("donkeu", 6);
+    printNums();
   }
 
   public static void printNTimes(String word, int N)
   {
-    // code solution here
+    for (int i = 0; i < word.length(); i++) //for each letter in the word... (loop)
+    {
+      String letter = word.substring(i,i+1);
+      for (int j = 0; j < N; j++)           // print that letter N times (loop)
+      {
+        System.out.print(letter);
+      }
+      System.out.println();
+    }
   }
+  
 
   public static void printNums()
   {
-    // code solution here
+    for (int i = 10; i > 0; i--)
+    {
+      
+      for (int j = 0; j < i; j++)
+      {
+        System.out.print(i + " ");
+      }
+      System.out.println();
+    }
   }
 
   public static void uprightNumberTriangle(int N)
